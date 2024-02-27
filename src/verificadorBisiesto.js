600cabd35e0b8d43f359836943f550cb4496b679
Diegodenac <diegocanedo917@gmail.com>
2024-02-27 05:37:39 -0400

    el codigo fue refactorizado

--- src/verificadorBisiesto.js
+++ src/verificadorBisiesto.js
@@ -1,9 +1,5 @@
 function esBisiesto(n) {
-  if(n%100 === 0 && n%400 !=0)
-    return false;
-  if(n%4===0)
-    return true;
-  return false
+  return (n%4===0) && !(n%100 === 0 && n%400 !=0)
 }
 
 export default esBisiesto;
