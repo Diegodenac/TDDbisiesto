49f224118a4b44945eb3ef08bfe3f7f0b23caae1
Diegodenac <diegocanedo917@gmail.com>
2024-02-27 05:13:58 -0400

    si es divisible entre 4 es bisiesto

--- src/verificadorBisiesto.test.js
+++ src/verificadorBisiesto.test.js
@@ -1,10 +1,13 @@
 import esBisiesto from "./verificadorBisiesto.js";
 
-describe("Verificar Bisiesto", () => {
+describe("Verificar si es Bisiesto", () => {
   it("Verificar si 4 es año bisiesto", () => {
     expect(esBisiesto(4)).toEqual(true);
   });
   it("Verificar si 2017 es año bisiesto", () => {
     expect(esBisiesto(2017)).toEqual(false);
+  });
+  it("Verificar si es divisible entre 4 es año bisiesto", () => {
+    expect(esBisiesto(8)).toEqual(true);
   });
 });
